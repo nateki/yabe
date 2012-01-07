e095480aa08b36794e769bc23b825f85a802ccbe
Kavinaesh Dheenadayalan <dkavinaesh@gmail.com>
2012-01-07 23:20:36 +0530

    day1 first screen

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,7 +2,7 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.play.*;
 import java.util.*;
 
 import models.*;
@@ -10,7 +10,15 @@ import models.*;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+          Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
     }
-
+@Before
+static void addDefaults() {
+    renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+    renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+}
 }
