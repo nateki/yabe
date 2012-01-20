d4597c817a436f71a2101e563a1b71fe9262f9fc
Kavinaesh Dheenadayalan <dkavinaesh@gmail.com>
2012-01-20 09:25:03 +0530

    day3 I am back basic admin area using CRUD

--- app/models/User.java
+++ app/models/User.java
@@ -1,14 +1,18 @@
 package models;
+
  
 import java.util.*;
 import javax.persistence.*;
  
 import play.db.jpa.*;
+import play.data.validation.*;
  
 @Entity
 public class User extends Model {
- 
+ @Email
+    @Required
     public String email;
+     @Required
     public String password;
     public String fullname;
     public boolean isAdmin;
